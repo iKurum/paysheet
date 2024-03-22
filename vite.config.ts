707351bcc6274eb25f06b6b5e41571e1774ca8e4
cyan <cyan@ikurum.cn>
2024-03-22 15:00:06 +0800

    打包优化

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { VitePWA } from "vite-plugin-pwa";
+import viteCompression from "vite-plugin-compression";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -24,5 +25,29 @@ export default defineConfig({
         ],
       },
     }),
+    viteCompression({
+      verbose: true,
+      disable: false,
+      threshold: 10240,
+      algorithm: "gzip",
+      ext: ".gz",
+    }),
   ],
+  build: {
+    //小于此阈值的导入或引用资源将内联为 base64 编码，以避免额外的 http 请求。设置为 0 可以完全禁用此项
+    assetsInlineLimit: 1024,
+    //启用/禁用 CSS 代码拆分
+    cssCodeSplit: true,
+    //构建后是否生成 source map 文件
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes("node_modules")) {
+            return id.toString().split("node_modules/")[1].split("/")[0];
+          }
+        },
+      },
+    },
+  },
 });
